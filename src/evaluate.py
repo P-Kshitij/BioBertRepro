6b668fbc7c532d7de10bb87b1cd36e6f46a5e2a7
root <kshitijparwani.mat18@itbhu.ac.in>
2020-11-06 09:20:34 +0000

    evaluate.py done, entity-level evaluation now working using seqeval.metrics

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,6 +4,7 @@ import joblib
 import torch
 from tqdm import tqdm
 from sklearn.metrics import classification_report
+from seqeval.metrics import classification_report as seqeval_classification_report
 import numpy as np
 
 import config
@@ -39,35 +40,12 @@ def evaluate(test_dataloader, model, device, num_tags, grouped_entities=False):
                 data[k] = v.to(device)
             data_without_group_masks = {k:v for k,v in data.items() if k!='group_masks'}
             logits, _ = model(**data_without_group_masks)
-            if grouped_entities==True:
-                for i,text in enumerate(data["ids"].cpu().numpy()):
-                    grouped_text = config.TOKENIZER.decode(
-                        text,
-                        skip_special_tokens = True
-                    )
-                    ungrouped_text = config.TOKENIZER.convert_ids_to_tokens(
-                        text,
-                        skip_special_tokens = True
-                    )
-                    print(data['group_masks'][0])
-                    print(type(data['group_masks']))
-                    print(grouped_text)
-                    print(ungrouped_text)
-                    print(len(ungrouped_text))
-                    print(len(data["mask"].cpu().numpy()[i]))
-                    print('target_tags: ', data['target_tags'][0,data['mask'].cpu().numpy()])
-                    break
-                    #print((np.array(grouped_text))[data["mask"].cpu().numpy()[i]])
-                    #print(np.array(grouped_text)))
 
             tags_pred = logits.argmax(2).cpu().numpy()
             mask_np = data['mask'].cpu().numpy()
             target_tags_np = data['target_tags'].cpu().numpy()
             group_masks = data['group_masks'].cpu().numpy()
             for idx, arr in enumerate(tags_pred):
-                # if idx==0:
-                #     print(list(arr[mask_np[idx,:]==1])[1:-1])
-                #     print(len(list(arr[mask_np[idx,:]==1])[1:-1]))
                 pred_tags = arr[mask_np[idx,:]==1][1:-1]
                 target_tags = (target_tags_np[idx, mask_np[idx,:]==1])[1:-1]
                 assert(len(pred_tags)==len(target_tags))
@@ -77,21 +55,14 @@ def evaluate(test_dataloader, model, device, num_tags, grouped_entities=False):
                     grouped_tags_ytrue = []
                     assert(len(group_mask_idx)==len(pred_tags))
                     i = 0
-                    while(i < len(group_mask_idx)
-                    ):
+                    while(i < len(group_mask_idx)):
                         group_idx = group_mask_idx[i]
                         target_group_tags = set()
                         pred_group_tags = set()
-                        if idx==0:
-                            print('starting i',i)
                         while(i<len(group_mask_idx) and group_mask_idx[i]==group_idx):
-                            if idx==0:
-                                print(i, group_mask_idx[i], group_idx)
                             pred_group_tags.add(pred_tags[i])
                             target_group_tags.add(target_tags[i])
                             i+=1
-                        if idx==0:
-                            print('ending i',i)
                         # Sanity check to ensure that when we rebuild from tokens, we get the same tags.
                         # Note that we just did reverse of this transform in dataset.py lines (29)
                         assert(len(target_group_tags)==1)
@@ -101,28 +72,32 @@ def evaluate(test_dataloader, model, device, num_tags, grouped_entities=False):
                             # Meaninng ['ata' , '##xia'] has tags ['B','B'] meaning we can combine
                             grouped_tags_ypred.append(pred_group_tags.pop())
                         else:
-                            # Here we have the different tags for different tokens of a word
-                            # Meaninng ['ata' , '##xia'] has tags ['B','O'] meaning we can must treat 
+                            # Here we have different tags for different tokens of a word
+                            # Meaninfrom seqeval.metrics import classification_reportfrom seqeval.metrics import classification_reportg ['ata' , '##xia'] has tags ['B','O'] meaning we can must treat 
                             # as a wrong answer and add an 'X' 
                             grouped_tags_ypred.append(3)
-                    print('idx: ',idx,'len',len(grouped_tags_ypred))
-                    print(grouped_tags_ypred)
-                    print(grouped_tags_ytrue)
-                    print(group_mask_idx)
-                    tags_ypred.extend(grouped_tags_ypred)
-                    tags_ytrue.extend(grouped_tags_ytrue)
-                    assert(len(tags_ytrue)==len(tags_ypred))
+                    # print('idx: ',idx,'len',len(grouped_tags_ypred))
+                    # print(grouped_tags_ypred)
+                    # print(grouped_tags_ytrue)
+                    # print(group_mask_idx)
+                    tags_ypred.append(grouped_tags_ypred)
+                    tags_ytrue.append(grouped_tags_ytrue)
+                    assert(len(grouped_tags_ypred)==len(grouped_tags_ytrue))
                 else:   
-                    tags_ypred.extend(list(pred_tags))
-                    tags_ytrue.extend(list(target_tags))
-                    assert(len(tags_ytrue)==len(tags_ypred))
-            return tags_ypred, tags_ytrue
+                    tags_ypred.append(list(pred_tags))
+                    tags_ytrue.append(list(target_tags))
+                    assert(len(pred_tags)==len(target_tags))
+            # Uncomment for quick evaluation on just 8 examples        
+            #return tags_ypred, tags_ytrue
 
     return tags_ypred, tags_ytrue
     
-
-
-
+def decode_transform(arr):
+    transform_dict = {0:'B',1:'I',2:'O',3:'X'}
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            arr[i][j] = transform_dict[arr[i][j]]
+    return arr
 
 if __name__ == "__main__":
     meta_data = joblib.load(config.METADATA_PATH)
@@ -137,5 +112,7 @@ if __name__ == "__main__":
     tags_ypred, tags_ytrue = evaluate(test_dataloader, model, device, num_tags, grouped_entities=True)
     # tags_ypred = enc_tags.inverse_transform(tags_ypred)
     # tags_ytrue = enc_tags.inverse_transform(tags_ytrue)
-    print(tags_ytrue,tags_ypred)
-    print(classification_report(tags_ytrue, tags_ypred))
+    tags_ypred = decode_transform(tags_ypred)
+    tags_ytrue = decode_transform(tags_ytrue)
+    # print(tags_ytrue,tags_ypred)
+    print(seqeval_classification_report(tags_ytrue, tags_ypred))
